524ab88195b806c8ddcbb3fce5471e08fe9634ad
Zuzek16 <zuzekzych@gmail.com>
2025-01-06 15:38:18 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Educational quiz about Poland
+
+Test your knowledge of the country and it's people.
+
+## Technologies used
+
+JavaScript, JSON
+
+## What did I learn, making this?
+
+I practiced using and making json files.
+
+ It was my first time reading from a file in JS that wasn't sent in by the user.
+
---
+++ script.js
@@ -0,0 +1,23 @@
+import data from '../questions.json';
+console.log(String(data));
+let points = 0;
+
+function start() {
+    
+}
+
+function anwser(isRight) {
+    
+}
+
+const file = new File([""], "questions.json", {
+    type: "application/json",
+  });
+
+  console.dir(file);
+const fr = new FileReader()
+
+console.log(fr.readAsText(file));
+
+  
+  
