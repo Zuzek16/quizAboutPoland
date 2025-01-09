8e006538cb2491bc1577688e257a99c0fbf71058
Zuzek16 <zuzekzych@gmail.com>
2025-01-09 22:26:04 +0100

    anwser verification; results screen

--- README.md
+++ README.md
@@ -8,13 +8,15 @@ JavaScript, JSON
 
 ## What did I learn, making this?
 
-I practiced using and making json files.
+I practiced using and making useful and easy to use json files.
 
 I had to read from a file in JS that wasn't sent in by the user.
 
 Promises came into action with the fetch() ajax function.
 
-I had to broden my knowlegde of promises and JS async.
+I had to broaden my knowlegde of promises and JS async.
+
+Rest of the app is JS DOM manipulation and functionality through JS.
 
 # Play here
 
--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-// import data from '../questions.json';
-// console.log(String(data));
 let points = 0;
-let question = 0;//counter
+let questionCounter = 0;
 let jsonData = [];
 var gotData = false;
 let hidden = "none";
 let shown = "block";
+
+let question = "";
+let options = {};
+let values = [];
+
+let hardMode = false;
+
+let longestWordID = 0;
+//fix percent whrn 100 it shows 1
+let anwsersOut = [];
+
 async function getJSONData() {
   await fetch('./questions.json')
 .then(res => res.json())
@@ -16,7 +25,7 @@ async function getJSONData() {
 })
 .finally(()=>{
   gotData = true;
-  printQuestion(question);
+  printQuestion(questionCounter);
   //still not perfect
 })
 }
@@ -26,18 +35,98 @@ function start() {
     document.getElementById("menu").style.display = hidden;
 
     document.getElementById("quiz").style.display = shown;
+
+ 
+    let widestEl = document.getElementById(longestWordID).clientWidth;
+    console.log(widestEl);
+    
+    for (let i = 0; i <= 3; i++) {
+      console.log(document.getElementById(i));
+      
+      document.getElementById(i).style.setProperty("width", ((widestEl+15) + "px"));
+    }
+
     
 }
 
 function anwser(isRight) {
+  if (isRight) {
+    points ++;    
+  } else if (hardMode){
+    //UPDATE?
+      console.log("SHOULDNT RUN YET");
+      
+      points --;
     
+  }
+
+  console.log(points);
+  
+  questionCounter ++;
+  printQuestion(questionCounter);
+
+}
+
+function checkAnwser(optionId) {
+  if (values[optionId]) {
+    anwser(true);
+  } else {
+    anwser(false);
+  }
+}
+
+function resultsScreen() {
+  loadAnwsers();
+  let percent = points/jsonData.length;
+  document.getElementById("quiz").style.setProperty("display", hidden);
+  document.getElementById("result").style.setProperty("display", shown);
+
+  document.getElementById("points").innerText = points + "/" + jsonData.length;
+  document.getElementById("percent").innerText = percent + " %"
+}
+
+function loadAnwsers() {
+  const htmlEl = document.getElementById("anwsersOut");
+
+  anwsersOut.forEach(q => {
+    let correctAnw = "";
+
+    console.log(q[1]);
+    q[1].forEach((anw, id)=>{
+
+      correctAnw += "<p class=";
+
+      if (q[2][id]) {
+        correctAnw += "'r'";
+      } else {
+        correctAnw += "'w'";
+      }
+      
+      correctAnw += ">" + anw + "</p>"
+
+        })
+
+    htmlEl.innerHTML += `
+    <div>
+
+    <p class='bold'>${q[0]}</p>
+    `+correctAnw+`</div>`;
+
+  })
+}
+
+function rewealAnwsers() {
+document.getElementById("anwsersOut").classList.toggle("reweal");
+  
 }
 
 function printQuestion(id) {
-  let question = jsonData[id].text; 
-  let options = (Object.keys(jsonData[id].options));
-  let values = [];
+  if (jsonData[id] != null) {
 
+    question = jsonData[id].text; 
+   options = (Object.keys(jsonData[id].options));
+
+   values = [];
   options.forEach(el => {
    values.push(jsonData[id].options[el]);
   });
@@ -46,15 +135,28 @@ function printQuestion(id) {
   console.log(options);
   console.log(values);
 
+  anwsersOut.push([question, options, values]);
+
   document.getElementById("question").innerText = question;
+
+  options.forEach((el, id) => {
+    if(el.length > options[longestWordID].length) {
+      longestWordID = id;
+    }
+  });
+
+  var element = document.getElementById(longestWordID),
+    style = window.getComputedStyle(element),
+    width = style.getPropertyValue("width");
 
   options.forEach((op, index) => {
     document.getElementById(`${index}`).innerText = op;     
   });
 
+  } else {
+    resultsScreen();
+  }
+   
 }
-
-//
-//HOW TO WAIT FOR PROMISES?!
 
 getJSONData();
